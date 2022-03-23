b33fb1497dd4e29523e5802112ac8c55ff520a65
Wesley Koech (205) <wesley.kiprop@8teq.co.ke>
2022-03-23 15:51:30 +0300

    Added list ingredient feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -51,3 +51,11 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """Test the string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Customer'
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
